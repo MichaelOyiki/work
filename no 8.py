05857fba76f88e986995c2c4f9d38352f1344091
MichaelOyiki <22moyiki.acc@lwetb.ie>
2025-09-02 14:37:38 +0100

    Add files via upload

---
+++ no 8.py
@@ -0,0 +1,12 @@
+days = int(50)
+hrs = int(24)
+mins = int(60*24)
+secs = int(60*60*24)
+
+total_hrs = int(hrs*50)
+total_mins = int(mins*24*50)
+total_secs = int(secs*60*24*50)
+
+print(total_hrs)
+print(total_mins)
+print(total_secs)
