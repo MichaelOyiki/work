55e683307e00796c148d2ecceea5eeeee181a311
MichaelOyiki <22moyiki.acc@lwetb.ie>
2025-09-02 13:49:00 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+_name = 'bob'
+age = 'twentyfive'
+print(_name,age)
+
+total = 25-11
+print(total)
+'''
+#remainder = total/name
+#print(remainder)
+'''
+'''
+#snake case
+any_word_
+'''
+#camelCase
+bankHolidayWeekend
