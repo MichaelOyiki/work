f228d023cdbef33fc0bc3a67fb01da3c4da6c618
MichaelOyiki <22moyiki.acc@lwetb.ie>
2025-09-09 13:52:57 +0100

    Update and rename no 5.py to assignment 1/no 5.py

---
+++ assignment 1/no 5.py
@@ -0,0 +1,3 @@
+pizzas_started = int(12)
+pizzas_eaten = int(4)
+pizzas_leftover = int(pizzas_started-pizzas_eaten)
--- no 5.py
+++
@@ -1,3 +0,0 @@
-pizzas_started = int(12)
-pizzas_eaten = int(4)
-pizzas_leftover = int(pizzas_started-pizzas_eaten)
